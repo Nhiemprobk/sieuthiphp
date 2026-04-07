8db14e856f13dd31c50ca64a7eb96667e9028c56
NhiemProBacKy <nhiemhoang1811@gmail.com>
2026-04-07 08:02:34 +0700

    first commit

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'name' => 'Lam Vu Hoang',
+            'email' => 'lambeobn@gmail.com',
+            'password' => Hash::make('123123123'),
+        ]);
+        DB::table('admins')->insert([
+            'name' => 'Nguyen Dai Cuong',
+            'email' => 'cuongbs@gmail.com',
+            'password' => Hash::make('1233'),
+        ]);
+        DB::table('admins')->insert([
+            'name' => 'Nguyen Anh Duc',
+            'email' => 'nguyenanhduc123456@gmail.com',
+            'password' => Hash::make('123123123'),
+        ]);
+    }
+}
