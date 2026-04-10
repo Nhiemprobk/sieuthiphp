f0d8ee34b6f4944615689ccec53fc65330bfdf0a
NhiemProBacKy <nhiemhoang1811@gmail.com>
2026-04-10 21:02:42 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,9 @@ class HomeController extends Controller
     public function index()
     {
         $products = Cache::remember('homepage_products', 600, function () {
-            return Product::where('status', 1)->get();
+            return Product::with(['product_category', 'promotion'])
+                          ->where('status', 1)
+                          ->get();
         });
 
         return view('home', compact('products'));
